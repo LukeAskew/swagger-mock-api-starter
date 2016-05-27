914105d61da43416dec184cf2cb83cdd61128bfe
Luke Askew <LukeAskew@users.noreply.github.com>
2016-05-27 17:07:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,10 @@ const mock = require('swagger-mock-api');
 const app = express();
 const specFile = path.join(__dirname, 'spec.yml');
 
-const spec = fs.readFileSync(specFile, 'utf-8');
-
 app.use(mock({
-	swaggerFile: specFile,
-	watch: true
-}))
-
-// app.use(SwaggerDocs.middleWare(spec, { path: '/api' }))
+  swaggerFile: specFile,
+  watch: true
+}));
 
 app.listen(4000, function () {
   console.log('API running on port 4000');
